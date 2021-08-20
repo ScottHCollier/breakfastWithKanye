db6fcdc6809f8990e4298377c0e8efb4c5b8b4ee
scott collier <scott.h.collier@gmail.com>
2021-08-20 15:22:46 +0100

    sass and script structures added

---
+++ public/javascript/scripts.js
@@ -0,0 +1,8 @@
+setInterval(() => {
+    // const badges = this.state.badges.slice();
+    // const coinFlip = Math.floor(Math.random() * 2);
+    // const randomIndex = Math.floor(Math.random() * badges.length);
+    // badges[randomIndex].color = this.getRandomColor();
+    // badges[randomIndex].rotate = rotate[coinFlip];
+    // this.setState({ badges });
+}, 125);
